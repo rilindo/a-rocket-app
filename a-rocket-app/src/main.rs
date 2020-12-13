5c670807187cf175a8dcc44f62654dd74fa2a8ef
Rilindo Foster <rilindo@monzell-labs.net>
2020-12-12 20:47:07 -0600

    Added initial code

---
+++ a-rocket-app/src/main.rs
@@ -0,0 +1,22 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: String, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, we need to talk about your coolness.", name)
+    }
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to my site"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![hello, index]).launch();
+}
